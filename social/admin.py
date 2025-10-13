a2d5deae42dae5dea67e0bf84e9b78e313540c1f
Juan Saenz Valiente <j2saenzvaliente@gmail.com>
2025-10-13 11:42:14 -0300

    modificacion:   social/models.py

--- social/admin.py
+++ social/admin.py
@@ -1,4 +1,6 @@
+from typing import Any
 from django.contrib import admin
+from django.http import HttpRequest
 from .models import Link
 
 
@@ -10,5 +12,10 @@ class LinkAdmin(admin.ModelAdmin):
     ordering = ('name',)
     readonly_fields = ('created_at', 'updated_at')
     prepopulated_fields = {'key': ('name',)}
+
+    def get_readonly_fields(self, request: HttpRequest, obj: Any | None = ...) -> list[str] | tuple[Any, ...]:
+        if request.user.groups.filter(name='Content Editors').exists():
+            return ('created_at', 'updated_at', 'key', 'name')
+        return super().get_readonly_fields(request, obj)
 
 admin.site.register(Link, LinkAdmin)
